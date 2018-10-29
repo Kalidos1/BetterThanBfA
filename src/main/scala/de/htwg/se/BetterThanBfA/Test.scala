418c7fbb2de0cae959b0c06305edb93320c8ecf4
Marvin Klett <Marvin.Klett@htwg-konstanz.de>
2018-10-29 11:33:28 +0100

    test

---
+++ src/main/scala/de/htwg/se/BetterThanBfA/Test.scala
@@ -0,0 +1,22 @@
+package de.htwg.se.BetterThanBfA
+
+import de.htwg.se.BetterThanBfA.model._
+import scala.io.StdIn.readLine
+
+object Test {
+
+  var test = new MapTest(3)
+
+  def main(args: Array[String]): Unit = {
+    var input: String = ""
+
+
+    do {
+      println("MapTest : " + test.toString)
+      input = readLine()
+      println("Input: " + input)
+    } while (input != "q")
+
+  }
+
+}
